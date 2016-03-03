8891a07ca56da481543e48aeacb1eb32c0e89caf
didierClin <didier.c62@numericable.fr>
2016-03-03 20:17:20 +0100

    added forms_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    # debugger                     #debugger byebug
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+private
+ def meth
+   #code
+ end
+
 end
